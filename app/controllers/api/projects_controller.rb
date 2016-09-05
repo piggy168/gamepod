569dd8c15d26058acb9d241b7c735fd357126d30
piggy168 <whaleking168@gmail.com>
2016-09-05 00:01:35 -0700

    form styling

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -6,13 +6,10 @@ class Api::ProjectsController < ApplicationController
 
   def create
     @project = Project.new(project_params)
-    debugger
     if @project.save
         rewards = params[:project][:reward].each{|key, value|  params[:project][:reward][key][:project_id]=@project.id}
-        debugger
         success = rewards.each {|key, reward|
           Reward.new(title: reward[:title], description: reward[:description], amount: reward[:amount].to_i, limit: reward[:limit].to_i, project_id: reward[:project_id]).save}
-        debugger
         if success.include?(false)
           render json: "error", status: 500
         else
