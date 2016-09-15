54579e7fac440734530ad2a6739a2914c6e93f61
piggy168 <whaleking168@gmail.com>
2016-09-14 22:35:52 -0700

    cloudinary

--- frontend/components/start_form/start_form.jsx
+++ frontend/components/start_form/start_form.jsx
@@ -31,7 +31,7 @@ class StartForm extends React.Component{
 
   upload(e){
     e.preventDefault();
-    cloudinary.openUploadWidget(CLOUDINARY_OPTIONS, function(error, results){
+    cloudinary.openUploadWidget({cloud_name: "dlszpthqv", upload_preset: "e7urfwyg"}, function(error, results){
       if(!error){
         this.setState({photo_url: results[0].url});
       }
